aadb9b847954aa6f0fe41fd16cc5232e6eec22ea
ewagner <ericwagnerpreston@gmail.com>
2020-09-06 20:56:50 -0500

    Parse JSON and create some more files

--- helloworld.py
+++ helloworld.py
@@ -1,22 +1,44 @@
 import os.path
 import requests
+import json
 
-def writeFileContents(file, responseText):
-    file.write(responseText)
+def createFile(filePath):
+    mode = "wt" if os.path.exists(filePath) else "a"
+    file = open(filePath, mode)
+    return file
+
+def writeFileContents(file, content):
+    file.write(content)
     file.close()
+
+def getTodoTitles(todos):
+    todoTitles = []
+    for todo in todos:
+        todoTitles.append(todo["title"])
+    return ",".join(todoTitles)
+
+def getTodoIds(todos):
+    todoIds = []
+    for todo in todos:
+        todoIds.append(str(todo["id"]))
+    return ",".join(todoIds)
 
 try: 
     response = requests.get('https://jsonplaceholder.typicode.com/todos')
 
     if response.status_code == 200:
-        filePath = "response.txt"
+        responseFile = createFile("response.txt")
+        writeFileContents(responseFile, response.text)
 
-        if os.path.exists(filePath):
-            file = open(filePath, "wt")
-            writeFileContents(file, response.text)
-        else:
-            file = open(filePath, "a")
-            writeFileContents(file, response.text)
+        todos = json.loads(response.text)
+
+        titlesFile = createFile("titles.txt")
+        titlesCsv = getTodoTitles(todos)
+        writeFileContents(titlesFile, titlesCsv)
+
+        todoIdsFile = createFile("todoIds.txt")
+        todoIdsCsv = getTodoIds(todos)
+        writeFileContents(todoIdsFile, todoIdsCsv)
     else:
         print('Oh no, we got a non 200 status code from our request :(')
 except Exception as e:
